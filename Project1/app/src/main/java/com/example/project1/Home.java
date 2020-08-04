48af29f815a62c1e2ebcaafd13d93929e27e4753
simranjit singh <parminder.kang.0710@gmail.com>
2020-08-04 13:54:32 -0400

    pizza Size Screen

--- Project1/app/src/main/java/com/example/project1/Home.java
+++ Project1/app/src/main/java/com/example/project1/Home.java
@@ -2,13 +2,23 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
-
+    ImageView pizza;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        pizza=findViewById(R.id.imageView3);
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToHome = new Intent(Home.this, Main2Activity.class);
+            }
+        });
     }
 }
