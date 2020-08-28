77b009ddc149daf411283a8872452cb3eac2d8a5
parminderkang0710 <parminder.kang.0710@gmal.com>
2020-08-28 15:41:20 -0400

    add pizza Activity

--- Project1/app/src/main/java/com/example/project1/pizza.java
+++ Project1/app/src/main/java/com/example/project1/pizza.java
@@ -1,8 +1,26 @@
 package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
 
 public class pizza extends AppCompatActivity {
 
