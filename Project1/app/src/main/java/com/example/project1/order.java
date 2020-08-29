4094a60044ee848f1cd83c63b315d5393e83b505
Dilpreet <dilpreetbhogal20@gmail.com>
2020-08-29 12:26:26 -0400

    order code finished

--- Project1/app/src/main/java/com/example/project1/order.java
+++ Project1/app/src/main/java/com/example/project1/order.java
@@ -1,14 +1,77 @@
 package com.example.project1;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class Order {
 
-public class order extends AppCompatActivity {
+    String id, pizza, size, total, userid;
+    int price, qty;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order);
+    public Order() {
+    }
+
+    public Order(String id, String pizza, String size, String total, String userid, int price, int qty) {
+        this.id = id;
+        this.pizza = pizza;
+        this.size = size;
+        this.total = total;
+        this.userid = userid;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(String pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
     }
 }
