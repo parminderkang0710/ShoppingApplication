396590733dfab2b2b539f99e9d3077e62046c254
Arashdeep <arashbhullar1504@gmail.com>
2020-08-29 15:05:29 -0400

    user source code

---
+++ Project1/app/src/main/java/com/example/project1/User.java
@@ -0,0 +1,66 @@
+package com.example.project1;
+
+public class User {
+
+    String id, name, email, address, mobile, city;
+
+    public User() {
+    }
+
+    public User(String id, String name, String email, String address, String mobile, String city) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.mobile = mobile;
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
